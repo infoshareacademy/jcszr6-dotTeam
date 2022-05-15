632571f5b45a3f5e79bc2d26526a5e03d762a38a
Łukasz_Gulczyński <lukasz.gulczynski90@gmail.com>
2022-05-16 00:22:38 +0200

    Finished Text_Menu, input check works.

--- PlanAndRide/PlanAndRide.GUI/Menu.cs
+++ PlanAndRide/PlanAndRide.GUI/Menu.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
 
 namespace PlanAndRide.GUI
 {
@@ -19,40 +14,42 @@ namespace PlanAndRide.GUI
             Console.WriteLine("3. Wejdź jako gość");
             Console.WriteLine("-------------------------");
 
-            var userInput = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(userInput))
+            do
             {
-                _inputMenu = int.Parse(userInput);
-            }
-            Console.WriteLine("-------------------------");
-            switch (_inputMenu)
-            {
-                case 1: 
-                    Console.WriteLine("\nTworzymy konto..."); 
-                    break;
+                GetUserInput();
+                if (_inputMenu > 0 && _inputMenu <= 3)
+                {
+                    switch (_inputMenu)
+                    {
+                        case 1:
+                            Console.WriteLine("\nTworzymy konto...");
+                            break;
 
-                case 2: 
-                    Console.WriteLine("\nWitaj zalogowany użytkowniku...");
-                    LoggedUserMenu();
-                    break;
+                        case 2:
+                            Console.WriteLine("\nWitaj zalogowany użytkowniku...");
+                            LoggedUserMenu();
+                            break;
 
-                case 3:
-                    Console.WriteLine("\nWitaj gościu...");
-                    GuestMenu();
+                        case 3:
+                            Console.WriteLine("\nWitaj gościu...");
+                            GuestMenu();
+                            break;
+                    }
                     break;
-
-                default:
-                    Console.WriteLine("\nProszę wybrać opcję 1 - 3 ");
-                    break;
-            }
-        }
+                }
+                else
+                {
+                    Console.WriteLine("\nProszę wybrac jedną z opcji menu");
+                }
+            } while (true);
+        } 
 
         public static void GuestMenu()
         {
             Console.Clear();
-            Console.WriteLine(
-                $"Jeśli chcesz skorzystać ze wszystkich funkcjonalności naszej aplikacji to " +
-                $"zachęcamy Cię do zalogowania się lub utworzenia konta");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine($"Jeśli chcesz skorzystać ze wszystkich funkcjonalności naszej " +
+                              $"aplikacji to \nzachęcamy Cię do zalogowania się lub utworzenia konta\n");
             Console.WriteLine("-------------------------");
             Console.WriteLine("1. Stwórz konto");
             Console.WriteLine("2. Zaloguj się");
@@ -63,45 +60,49 @@ namespace PlanAndRide.GUI
             Console.WriteLine("5. Wyjdź z aplikacji:");
             Console.WriteLine("-------------------------");
 
-            var userInput = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(userInput))
+            do
             {
-                _inputMenu = int.Parse(userInput);
-            }
-            Console.WriteLine("-------------------------");
-            switch (_inputMenu)
-            {
-                case 1:
-                    Console.WriteLine("Tworzymy konto...");
-                    break;
+                _inputMenu = 0;
+                GetUserInput();
+                if (_inputMenu > 0 && _inputMenu <= 5)
+                {
+                    switch (_inputMenu)
+                    {
+                        case 1:
+                            Console.WriteLine("Tworzymy konto...");
+                            break;
 
-                case 2:
-                    Console.WriteLine("\nWitaj zalogowany użytkowniku (nickname)...");
-                    LoggedUserMenu();
-                    break;
+                        case 2:
+                            Console.WriteLine("\nWitaj zalogowany użytkowniku (nickname)...");
+                            LoggedUserMenu();
+                            break;
 
-                case 3:
-                    Console.WriteLine("Lista wyszukanych tras...");
-                    break;
+                        case 3:
+                            Console.WriteLine("Lista wyszukanych tras...");
+                            break;
 
-                case 4:
-                    Console.WriteLine("Lista wyszukanych eventów...");
-                    break;
+                        case 4:
+                            Console.WriteLine("Lista wyszukanych eventów...");
+                            break;
 
-                case 5:
-                    Console.WriteLine("Dziękujemy za skorzystanie z aplikacji Plan&Ride...");
-                    Environment.Exit(0);
+                        case 5:
+                            Console.WriteLine("Dziękujemy za skorzystanie z aplikacji Plan&Ride...");
+                            Environment.Exit(0);
+                            break;
+                    }
                     break;
-
-                default:
-                    Console.WriteLine("Proszę wybrać opcję 1 - 5 ");
-                    break;
-            }
+                }
+                else
+                {
+                    Console.WriteLine("\nProszę wybrac jedną z opcji menu");
+                }
+            } while (true);
         }
 
         public static void LoggedUserMenu()
         {
-            Console.Clear();
+            Console.Clear(); 
+            Console.WriteLine("Zostałeś zalogowany jako {nickname}");
             Console.WriteLine("-------------------------");
             Console.WriteLine("1. Edytuj konto");
             Console.WriteLine("2. Wyloguj się");
@@ -121,75 +122,91 @@ namespace PlanAndRide.GUI
             Console.WriteLine("14. Wyjdź");
             Console.WriteLine("-------------------------");
 
-            var userInput = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(userInput))
+            do
             {
-                _inputMenu = int.Parse(userInput);
-            }
-            Console.WriteLine("-------------------------");
-            switch (_inputMenu)
+                _inputMenu = 0;
+                GetUserInput();
+                if (_inputMenu > 0 && _inputMenu <= 14)
+                {
+                    switch (_inputMenu)
+                {
+                    case 1:
+                        Console.WriteLine("Edytujemy konto...");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Wylogowałeś się...");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Tworzymy trasę...");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Lista wyszukanych tras...");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Edytujemy trasę...");
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Usuwamy trasę...");
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Udostępniamy trasę...");
+                        break;
+
+                    case 8:
+                        Console.WriteLine("Oceniamy trasę...");
+                        break;
+
+                    case 9:
+                        Console.WriteLine("Przeglądamy historię tras...");
+                        break;
+
+                    case 10:
+                        Console.WriteLine("Tworzymy event...");
+                        break;
+
+                    case 11:
+                        Console.WriteLine("Wyszukujemy event...");
+                        break;
+
+                    case 12:
+                        Console.WriteLine("Edytujemy event...");
+                        break;
+
+                    case 13:
+                        Console.WriteLine("Ustawiamy powiadomienia...");
+                        break;
+
+                    case 14:
+                        Console.WriteLine("Dziękujemy za skorzystanie z aplikacji Plan&Ride...");
+                        Environment.Exit(0);
+                        break;
+                }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nProszę wybrac jedną z opcji menu");
+                }
+            } while (true);
+        }
+
+        public static void GetUserInput()
+        {
+            do
             {
-                case 1:
-                    Console.WriteLine("Tworzymy konto...");
+                var userInput = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(userInput) && int.TryParse(userInput, out int value))
+                {
+                    _inputMenu = int.Parse(userInput);
                     break;
-
-                case 2:
-                    Console.WriteLine("Witaj zalogowany użytkowniku...");
-                    break;
-
-                case 3:
-                    Console.WriteLine("Tworzymy trasę...");
-                    break;
-
-                case 4:
-                    Console.WriteLine("Lista wyszukanych tras...");
-                    break;
-
-                case 5:
-                    Console.WriteLine("Edytujemy trasę...");
-                    break;
-
-                case 6:
-                    Console.WriteLine("Usuwamy trasę...");
-                    break;
-
-                case 7:
-                    Console.WriteLine("Udostępniamy trasę...");
-                    break;
-
-                case 8:
-                    Console.WriteLine("Oceniamy trasę...");
-                    break;
-
-                case 9:
-                    Console.WriteLine("Przeglądamy historię tras...");
-                    break;
-
-                case 10:
-                    Console.WriteLine("Tworzymy event...");
-                    break;
-
-                case 11:
-                    Console.WriteLine("Wyszukujemy event...");
-                    break;
-
-                case 12:
-                    Console.WriteLine("Edytujemy event...");
-                    break;
-
-                case 13:
-                    Console.WriteLine("Ustawiamy powiadomienia...");
-                    break;
-
-                case 14:
-                    Console.WriteLine("Dziękujemy za skorzystanie z aplikacji Plan&Ride...");
-                    Environment.Exit(0);
-                    break;
-
-                default:
-                    Console.WriteLine("Proszę wybrać opcję 1 - 14 ");
-                    break;
-            }
+                }
+            } while (!true);
         }
     }
 }
