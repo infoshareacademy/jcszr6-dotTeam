39addeac508cdba548bf0c79a88e0ad185c10da5
Rafal <rafalmika133@gmail.com>
2022-06-15 21:58:41 +0200

    Add RouteRepository

---
+++ PlanAndRide/PlanAndRide.BusinessLogic/RouteRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanAndRide.BusinessLogic
+{
+    public class RouteRepository
+    {
+        private List<Route> _routes;
+
+        public void AddRoute(Route route)
+        {
+            _routes.Add(route);
+        }
+        public List<Route> GetAllRoutes()
+        {
+            return _routes;
+        }
+    }
+}
