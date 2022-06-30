6e3da7e32c422f152b40ac80695edc209955f763
Andrzej Czarnecki <andrzej_czarnecki@poczta.fm>
2022-06-30 20:44:11 +0200

    adding folder Events in view and controler

---
+++ PlanAndRide/PlanAndRide.Web/Controllers/Events/EventsController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlanAndRide.BusinessLogic;
+
+
+namespace PlanAndRide.Web.Controllers.Events
+{
+    public class EventsController : Controller
+    {
+        // GET: EventsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EventsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EventsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EventsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EventsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
