67233e4550eb0818a235c49935f09d2731c23ea4
Andrzej Czarnecki <andrzej_czarnecki@poczta.fm>
2022-07-03 12:39:58 +0200

    Change RideRepository

--- PlanAndRide/PlanAndRide.BusinessLogic/RideRepository.cs
+++ PlanAndRide/PlanAndRide.BusinessLogic/RideRepository.cs
@@ -9,25 +9,77 @@ using Environment = System.Environment;
 
 namespace PlanAndRide.BusinessLogic
 {
-    public static class RideRepository: IRepository<Ride>
+    public  class RideRepository: IRepository<Ride>
     {
-        private static List<Ride> rides = new List<Ride>();
-
-        static RideRepository()
+        private List<Ride> _ride;
+        public RideRepository()
         {
-            var json = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "data.json"));
-            rides = JsonConvert.DeserializeObject<List<Ride>>(json);
+            _ride = new List<Ride>();
         }
-
-        public static List<Ride> GetAllRides()
+        public Ride? Get(int id)
         {
-            return rides;
+            try
+            {
+                return _ride.FirstOrDefault(r => r.Id == id);
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Unique key violaton: Ride Id:{id}");
+            }
         }
-
-        public static void AddRide(Ride ride)
+        public IEnumerable<Ride> GetAll()
         {
-            rides.Add(ride);
+            return _ride;
+        }        
+        public void Add(Ride ride)
+        {
+            if(_ride.Count > 0)
+            {
+                ride.Id = _ride.Max(r=>r.Id)+1;
+            }
+            else
+            {
+                ride.Id = 1;
+            }
+            _ride.Add(ride);
         }
+        public void Update(int id, Ride ride)
+        {
+            var existingRide= Get(id);
+            if(existingRide != null)
+            {
+                throw new RecordNotFoundException($"Ride Id:{id} not found in repository");
+            }
+            existingRide.Name = ride.Name;
+            existingRide.Date = ride.Date;
+            existingRide.IsPrivate = ride.IsPrivate;
+            existingRide.Route = ride.Route;
+        }
+        public void Delete(int id)
+        {
+            _=_ride.Remove(Get(id));
+        }
+        public IEnumerable<Ride> FindByName(string name)
+        {
+            return _ride.Where(r=>r.Name.ToLower()==name.Trim().ToLower));
+        }
+        //private static List<Ride> rides = new List<Ride>();
+
+        //static RideRepository()
+        //{
+        //    var json = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "data.json"));
+        //    rides = JsonConvert.DeserializeObject<List<Ride>>(json);
+        //}
+
+        //public static List<Ride> GetAllRides()
+        //{
+        //    return rides;
+        //}
+
+        //public static void AddRide(Ride ride)
+        //{
+        //    rides.Add(ride);
+        //}
 
     }
 }
