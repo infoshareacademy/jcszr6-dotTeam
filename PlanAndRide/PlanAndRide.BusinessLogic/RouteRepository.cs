718e13256c4523b2cee89b0113098486ee311e80
Andrzej Czarnecki <andrzej_czarnecki@poczta.fm>
2022-07-10 12:18:39 +0200

    add

---
+++ PlanAndRide/PlanAndRide.BusinessLogic/RouteRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanAndRide.BusinessLogic
+{
+    public class RouteRepository : IRepository<Route>
+    {
+        private List<Route> _routes;
+        public RouteRepository()
+        {
+            _routes = new List<Route>();
+        }
+        public Route? Get(int id)
+        {
+            try
+            {
+                return _routes.SingleOrDefault(r => r.Id == id);
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Unique key violaton: Route ID:{id}");
+            }
+        }
+        public IEnumerable<Route> GetAll()
+        {
+            return _routes;
+        }
+        public void Add(Route route)
+        {
+            if (_routes.Count > 0)
+            {
+                route.Id = _routes.Max(r => r.Id) + 1;
+            }
+            else
+            {
+                route.Id = 1;
+            }
+            _routes.Add(route);
+        }
+        public void Update(int id, Route route)
+        {
+            var existingRoute = Get(id);
+            if (existingRoute == null)
+            {
+                throw new RecordNotFoundException($"Route ID:{id} not found in repository");
+            }
+            existingRoute.Name = route.Name;
+            existingRoute.StartingPosition = route.StartingPosition;
+            existingRoute.DestinationPosition = route.DestinationPosition;
+            existingRoute.Description = route.Description;
+            existingRoute.ShareRoute = route.ShareRoute;
+            existingRoute.IsPrivate = route.IsPrivate;
+
+        }
+
+        public void Delete(int id)
+        {
+           _ = _routes.Remove(Get(id));
+        }
+        public IEnumerable<Route> FindByName(string name)
+        {
+            return _routes.Where(r => r.Name.ToLower() == name.Trim().ToLower());
+        }
+
+
+    }
+}
---
+++ PlanAndRide/PlanAndRide.Web/Controllers/RouteController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlanAndRide.BusinessLogic;
+
+namespace PlanAndRide.Web.Controllers
+{
+    public class RouteController : Controller
+    {
+        private readonly RouteRepository _routeRepository;
+
+        public RouteController(RouteRepository routeRepository)
+        {
+            _routeRepository = routeRepository;
+        }
+        // GET: RouteController
+        public ActionResult Index()
+        {
+
+            return View(_routeRepository.GetAll());
+        }
+
+        // GET: RouteController/Details/5
+        public ActionResult Details(int id)
+        {
+            var route = _routeRepository.Get(id);
+            if (route != null)
+            {
+                return View(route);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: RouteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RouteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BusinessLogic.Route route)
+        {
+            ModelState.Remove("Reviews");
+            if (!ModelState.IsValid)
+            {
+                return View(route);
+            }
+            _routeRepository.Add(route);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: RouteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var route = _routeRepository.Get(id);
+            if (route != null)
+            {
+                return View(route);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: RouteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, BusinessLogic.Route route)
+        {
+            ModelState.Remove("Reviews");
+            if (!ModelState.IsValid)
+            {
+                return View(route);
+            }
+            try
+            {
+                _routeRepository.Update(id, route);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RouteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var route= _routeRepository.Get(id);
+            if (route != null)
+            {
+                return View(route);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: RouteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, BusinessLogic.Route route)
+        {
+            _routeRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
