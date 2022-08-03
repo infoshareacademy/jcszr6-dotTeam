62f3747fdc2652e6e9552467fea9e4e1fe286599
Rafal <rafalmika133@gmail.com>
2022-08-03 08:40:57 +0200

    Autocomplete script move to site.js, add form partial view in Create

--- PlanAndRide/PlanAndRide.Web/wwwroot/js/site.js
+++ PlanAndRide/PlanAndRide.Web/wwwroot/js/site.js
@@ -3,4 +3,21 @@
 
 // Write your JavaScript code.
 
+function AutocomplePlaces () {
+    const originInput = new google.maps.places.Autocomplete(document.getElementById('StartingLocation'));
+    google.maps.event.addListener(originInput, 'place_changed', function () {
+        const placeDetails = originInput.getPlace();
+        document.getElementById('StartingLocation').value = placeDetails.formatted_address;
+        document.getElementById('StartingLatitude').value = placeDetails.geometry.location.lat();
+        document.getElementById('StartingLongitude').value = placeDetails.geometry.location.lng();
+    });
+
+    const destinationInput = new google.maps.places.Autocomplete(document.getElementById('DestinationLocation'));
+    google.maps.event.addListener(destinationInput, 'place_changed', function () {
+        const placeDetails = destinationInput.getPlace();
+        document.getElementById('DestinationLocation').value = placeDetails.formatted_address;
+        document.getElementById('DestinationLatitude').value = placeDetails.geometry.location.lat();
+        document.getElementById('DestinationLongitude').value = placeDetails.geometry.location.lng();
+    });
+}
 
