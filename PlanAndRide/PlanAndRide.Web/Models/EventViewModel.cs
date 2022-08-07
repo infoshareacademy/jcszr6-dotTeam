b402916d7d58209e63aceaabc019ec6d86c79523
Andrzej Czarnecki <andrzej_czarnecki@poczta.fm>
2022-08-07 14:48:40 +0200

    Test to create modelview event to create drop list

---
+++ PlanAndRide/PlanAndRide.Web/Models/EventViewModel.cs
@@ -0,0 +1,38 @@
+﻿using PlanAndRide.BusinessLogic;
+using PlanAndRide.Database;
+using System.ComponentModel.DataAnnotations;
+namespace PlanAndRide.Web.Models
+{
+    public class EventViewModel
+    {
+        public BusinessLogic.Ride Ride { get; }
+        public Database.Repository.RouteRepository RouteRepository { get; }
+       
+          
+      public int Id
+        {
+            get { return Ride.Id; }
+        }
+        [Required]
+      public string Name
+        {
+            get { return Ride.Name; }
+            set { Ride.Name = value; }
+        }
+        public string? Description
+        {
+            get { return Ride.Description; }
+            set { Ride.Description = value; }
+        }
+        public bool ShareRoute
+        {
+            get { return Ride.ShareRide; }
+            set { Ride.ShareRide = value; }
+        }
+        public bool IsPrivate
+        {
+            get { return Ride.IsPrivate; }
+            set { Ride.IsPrivate = value; }
+        }
+    }
+}
