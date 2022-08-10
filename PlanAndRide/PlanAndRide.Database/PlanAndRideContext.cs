ed74d083bd13a29955c7366a1bf5a3e368070474
Rafal <rafalmika133@gmail.com>
2022-08-10 22:25:06 +0200

    Add DbContext

---
+++ PlanAndRide/PlanAndRide.Database/PlanAndRideContext.cs
@@ -0,0 +1,27 @@
+﻿using GeoCoordinatePortable;
+using Microsoft.EntityFrameworkCore;
+using PlanAndRide.BusinessLogic;
+
+namespace PlanAndRide.Database
+{
+    public class PlanAndRideContext:DbContext
+    {
+        public DbSet<Ride> Rides { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<GeoCoordinate> GeoCoordinates { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=localhost;Database=PlanAndRideDb;Trusted_Connection=True;MultipleActiveResultSets=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+    }
+}
