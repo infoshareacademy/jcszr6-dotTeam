a945a8780789d5016fbe3356e92e0051fdb476ad
Rafal <rafalmika133@gmail.com>
2022-08-12 18:13:50 +0200

    Add migration geocoordinates default values

---
+++ PlanAndRide/PlanAndRide.Database/Migrations/20220811182749_AddGeoCoordinateDefaultValues.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanAndRide.Database.Migrations
+{
+    public partial class AddGeoCoordinateDefaultValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "VerticalAccuracy",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Speed",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "HorizontalAccuracy",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Course",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Altitude",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "VerticalAccuracy",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldDefaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Speed",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldDefaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "HorizontalAccuracy",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldDefaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Course",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldDefaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Altitude",
+                table: "GeoCoordinates",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldDefaultValue: 0.0);
+        }
+    }
+}
