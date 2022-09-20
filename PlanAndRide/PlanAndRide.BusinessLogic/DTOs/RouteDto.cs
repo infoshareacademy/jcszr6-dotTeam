5367cd9a59f7ffc35c45e166f7c4c72fcb0187c7
Rafal <rafalmika133@gmail.com>
2022-09-20 14:06:40 +0200

    add route validation attributes

--- PlanAndRide/PlanAndRide.BusinessLogic/DTOs/RouteDto.cs
+++ PlanAndRide/PlanAndRide.BusinessLogic/DTOs/RouteDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,20 @@ namespace PlanAndRide.BusinessLogic
     public class RouteDto
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
         public User? User { get; set; }
+        [Required(ErrorMessage="Please enter a starting location")]
+        public string? StartingLocation { get; set; }
+        [Required(ErrorMessage = "Please enter a target location")]
+        public string? DestinationLocation { get; set; }
         public GeoCoordinate? StartingPosition { get; set; }
         public GeoCoordinate? DestinationPosition { get; set; }
         public string? EncodedGoogleMapsPath { get; set; }
         public string? EncodedGoogleMapsWaypoints { get; set; }
         public string? StartingCity { get; set; }
         public string? DestinationCity { get; set; }
+        [MaxLength(200)]
         public string? Description { get; set; }
         public bool ShareRoute { get; set; }
         public bool IsPrivate { get; set; }
