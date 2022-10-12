0795d8c54741d6046378c43dd1fa00751aee2055
Andrzej Czarnecki <andrzej_czarnecki@poczta.fm>
2022-10-12 05:37:41 +0200

    Adding EventDTO, enum StatusList, change IRideService, RideService, EventController

---
+++ PlanAndRide/PlanAndRide.BusinessLogic/DTOs/EventDto.cs
@@ -0,0 +1,24 @@
+﻿using PlanAndRide.BusinessLogic;
+using System.ComponentModel.DataAnnotations;
+namespace PlanAndRide.BusinessLogic
+{
+    public class EventDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+        public string? RouteId { get; set; }
+        public string? RouteName { get; set; }
+
+        //public IEnumerable<BusinessLogic.Route>? Routes { get; set; }
+        public Route Route { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool ShareRide { get; set; }
+        public bool IsPrivate { get; set; }
+        public string? StatusRide { get; set; }
+    }
+}
---
+++ PlanAndRide/PlanAndRide.BusinessLogic/Enums/StatusList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanAndRide.BusinessLogic.Enums
+{
+    public enum StatusList
+    {
+        [Display(Name = "Unknown")] Unknown,
+        [Display(Name = "Is coming")] Comming,
+        [Display(Name = "Right now")] Right_Now,
+        [Display(Name = "Completed")] Completed,
+        [Display(Name = "Archived")] Archived
+    }
+}
